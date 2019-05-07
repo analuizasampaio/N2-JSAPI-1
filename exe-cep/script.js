efd5653c35a56af1f53090c2091e4b7aa5c6e153
analuizasampaio <sampaioaanaluiza@gmail.com>
2019-05-06 22:14:59 -0300

    exercicio CEP completo

--- exe-cep/script.js
+++ exe-cep/script.js
@@ -1,15 +1,40 @@
-let cep = document.getElementById('cep')
-let cep = EU NÃO SEI COMO FAZER ISSO 
-console.log(cep)
+const cep = document.getElementById('cep')
+console.log(cep.value)
 
+cep.addEventListener('focusout', ()=> {
+    console.log('focusout')
+    fetch('https://viacep.com.br/ws/'+ cep.value + '/json')
+    
 
-fetch(onmouseleave = 'https://viacep.com.br/ws/'+ cep + '/json')
-.then(function(response){
+    .then(function(response){
     return response.json()
+    })
+    .then(function(data){
+        console.log(cep.value)
+
+        // let estado = document.getElementById('estado')
+        // estado.value = data.uf
+        // console.log(data.uf)
+
+        // let cidade = document.getElementById('cidade')
+        // cidade.value = data.localidade
+        // console.log(data.localidade)
+        
+        // let bairro = document.getElementById('bairro')
+        // bairro.value = data.bairro
+        // console.log(data.bairro)
+
+        // let endereco = document.getElementById('endereco')
+        // endereco.value = data.logradouro
+
+        document.getElementById('estado').value = data.uf
+        document.getElementById('cidade').value = data.localidade
+        document.getElementById('bairro').value = data.bairro
+        document.getElementById('endereco').value = data.logradouro
+        
+    })
+    .catch(function(erro){
+        console.log(erro)
+    })
 })
-.then(function(data){
-    console.log(cep)
-})
-.catch(function(erro){
-    console.log(erro)
-})
+
